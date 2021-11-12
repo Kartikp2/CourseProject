bded310ce94b8505994e721708cd98e1f03d4a4f
selvaganapathyt <66457719+selvaganapathyt@users.noreply.github.com>
2021-11-12 01:06:15 -0500

    fixed the script to pull the video id from the metadata

---
+++ segment_extractor.py
@@ -0,0 +1,127 @@
+import re
+import os
+import csv
+import glob
+import json
+
+# convert time to seconds
+def get_time_as_seconds(time):
+    time = time.split(",")[0] # ignoring the milliseconds
+    hr, min, sec = map(int, time.split(':'))
+    return hr * 60 * 60 + min * 60 + sec
+    
+
+# extract segment details from segment_raw_txt
+def get_segment_details(course_metadata, course_id, week_nbr, video_id, segment_raw_txt):
+    segment = {}
+
+    segment_content = segment_raw_txt.split("\n")
+    seg_id = int(segment_content[0]) #cue
+    timeline = segment_content[1].split(" --> ")
+
+    key    = str(course_id) + "_" + str(week_nbr) + "_" + str(video_id) + "_" + str(seg_id)
+    segment["key"] = key
+    segment["course_id"] = course_id
+    segment["week_nbr"] = week_nbr
+    segment["video_id"] = video_id
+
+    segment["timeline_start"] = timeline[0]
+    segment["timeline_end"]   = timeline[1]
+    segment["segment_nbr"]    = seg_id
+    segment["segment_txt"]    = segment_content[2]
+    cm = course_metadata[course_id][str(week_nbr) + "." + str(video_id)]
+    
+    lesson_id = cm["id"]
+    video_title = cm["title"]
+
+    segment["segment_link"]   = "https://www.coursera.org/learn/cs-410/lecture/" + lesson_id + "?t=" + str(get_time_as_seconds(timeline[0]))
+    segment["video_title"]    = video_title
+    
+    return segment
+
+
+# persist the segment content to a csv file
+def write_segments_to_csv(segments):
+    csv_columns = ["key", "course_id", "week_nbr", "video_id", "video_title", "timeline_start", "timeline_end", "segment_nbr", "segment_link", "segment_txt"]
+    try:
+        with open("courseera_video_segments.csv", 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in segments:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
+
+# return segments per week
+def get_segments(course_metadata, course_id, video_id, week_nbr, srt_file):
+    with open(srt_file, 'r') as f:
+        content = f.read()
+
+    # split by segments
+        segments = re.split("\n\n", content)
+        seg_content_list = []
+    
+        for segment in segments :
+            seg_content = get_segment_details(course_metadata, course_id, week_nbr, video_id, segment)
+            seg_content_list.append(seg_content)
+
+    return seg_content_list
+
+
+def load_sylabus_metadata(course_id, sylabus_as_json):
+    course_metadata = {}
+    syb_meta_dict = {}
+
+    with open(sylabus_as_json, 'r') as f:
+        content = f.read()
+        course_json = json.loads(content)
+
+        for item in course_json["linked"]["onDemandCourseMaterialItems.v2"]:
+            if (item["contentSummary"]["typeName"] == "lecture"):
+                if item["name"].startswith("Lesson"):
+                    s = item["name"].replace("Lesson ", "")
+                    week_video   = s.split(":")[0]
+                    video_title   = s.split(":")[1]
+
+                    syb_meta_dict[week_video] = {"id" : item["id"], "title" : video_title}
+                else:
+                    name = item["name"]
+                    week_video   = name.split(" ")[0]
+                    video_title  = name.split(" ")[1]
+
+                    if "." in week_video:
+                        syb_meta_dict[week_video] = {"id" : item["id"], "title" : video_title}
+
+        course_metadata[course_id] = syb_meta_dict
+
+    return course_metadata
+
+
+
+def main():
+    
+    seg_content_final_list = []
+    course_id = "cs-410"
+    scan_dir = "courseera-dl/cs-410/"
+    srt_files = glob.glob(scan_dir + '**/*.srt', recursive=True)
+    
+    course_metadata = load_sylabus_metadata(course_id, "courseera-dl/cs-410-syllabus-raw.json")
+
+    for srt in srt_files:
+        video_id = int(os.path.basename(srt).split("_")[0])
+        week_nbr = int(os.path.dirname(srt).split("/")[-2].split("_")[0])
+
+        seg_content_srt = get_segments(course_metadata, course_id, video_id, week_nbr, srt)
+        seg_content_final_list.extend(seg_content_srt)
+            
+            # print(os.path.basename(srt) + " --> " +  str(len(seg_content_final_list)))
+            # print(os.path.dirname(srt).split("/")[3] + "==> " + str(video_id) + " -- > " + str(video_id.isdigit()))
+    
+
+    # write the final output to a csv
+    seg_content_final_list.sort(key=lambda a : (a["course_id"], a["week_nbr"], a["video_id"], a["segment_nbr"]))
+    write_segments_to_csv(seg_content_final_list)
+
+
+if __name__ == "__main__":
+    main()
